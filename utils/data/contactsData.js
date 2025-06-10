6225fabc3e9803ddfc61b483e517c6ec71c8d387
ThengdeNitin <nitinthengde14368@gmail.com>
2025-06-10 23:22:52 +0530

    added images,components

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,8 @@
 export const contactsData = {
   email: 'nitinthengde14368@gmail.com',
   phone: '+91 9689691657',
-  address: ' ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  devUsername: "said7388"
+  address: 'Iccha Building, Lane no.7, Kharadi Pune ',
+  github: 'https://github.com/ThengdeNitin',
+  facebook: 'https://www.facebook.com/nitin.thengade',
+  linkedIn: 'https://www.linkedin.com/in/nitin-thengde016/'
 }
