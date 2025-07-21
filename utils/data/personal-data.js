7ad19a33b4b619994f5666a420a9ae39103f4bbc
ThengdeNitin <nitinthengde14368@gmail.com>
2025-07-22 00:10:04 +0530

    Bug fixed

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,10 +5,10 @@ export const personalData = {
   description: "My name is NITIN THENGDE. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'nitinthengde14368@gmail.com',
   phone: '+91 9689691657',
-  address: 'Iccha BUilding, Lane No.7, Kharadi Pune -411014',
+  address: 'Iccha BUilding, Lane No.7, Kharadi Pune - 411014',
   github: 'https://github.com/ThengdeNitin',
   facebook: 'https://www.facebook.com/nitin.thengade',
   linkedIn: 'https://www.linkedin.com/in/nitin-thengde016/',
   leetcode: "https://leetcode.com/u/ThengdeNitin16/",
-  resume: "https://drive.google.com/drive/u/0/folders/1gSGcHkI0-OrHGSQqS0n7oQUZTDiOc8lo"
+  resume: "https://drive.google.com/file/d/1xf1zqhLVqrADLIP-4AFf_TmCmSDpYN_A/view?usp=sharing"
 }
